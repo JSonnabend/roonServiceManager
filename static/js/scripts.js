a406b764bece757d430ede78feaa5a295e58d4f3
jeff <jeff@sonnabendfamily.org>
2023-03-03 07:58:32 -0600

    adjust terminal height when soft keyboard activated

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,6 +5,15 @@ $(function() {
     };
     var commands = ['restart', 'status', 'zones &#91;zone_name|zone_number&#93;', 'list &#91;zones&#93;', 'log', 'ping', 'settings', 'cls', 'help'];
 
+    // resize event listener to detect change in screen height
+    window.addEventListener("resize", (e) => { setTimeout(function () {
+        let viewheight = window.visualViewport.height;
+        let viewwidth = $(window).width();
+        $('#terminal').css('height',viewheight + "px");
+        $('#terminal').css('width',viewwidth + "px");
+        $('body').css('height',viewheight + "px");
+        }, 300);
+      });
 
 
     $('#terminal').terminal([
